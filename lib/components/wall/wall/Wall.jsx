050dd158c4010ac73e1ab1cb5f430570d0276443
Jean-Yves Delmotte <jean-yves.delmotte@greencom-networks.com>
2015-12-26 16:42:08 +0100

    bug fix publish postsByWall + link posts for admin

--- lib/components/wall/wall/Wall.jsx
+++ lib/components/wall/wall/Wall.jsx
@@ -11,7 +11,8 @@ WallComponent = React.createClass({
     getMeteorData() {
         return {
             readyForWall: Meteor.subscribe('wallById', this.props.id).ready(),
-            wall: Walls.findOne({})
+            wall: Walls.findOne({}),
+            currentUser: Meteor.user()
         }
     },
 
@@ -63,6 +64,11 @@ WallComponent = React.createClass({
                             <div className="wall-geolocation">{self.data.wall.loc.lat}, {self.data.wall.loc.lon}</div>
                         </div>
                         : null
+                    }
+                    {
+                        self.data.currentUser && self.data.currentUser.profile.roles.indexOf('admin') >= 0 ?
+                            <div><a href={"/walls/" + this.props.id + "/posts"}>voir les posts</a></div>
+                            :null
                     }
                     {
                         self.data.wall && self.data.wall.pictures.length > 0 ?
