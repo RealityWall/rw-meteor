19b5985faeecf1a33dd8af3dbe351908cb528341
Jean-Yves Delmotte <jean-yves.delmotte@greencom-networks.com>
2016-01-03 18:12:07 +0100

    add social config

---
+++ server/social-config.js
@@ -0,0 +1,9 @@
+ServiceConfiguration.configurations.remove({
+    service: 'facebook'
+});
+
+ServiceConfiguration.configurations.insert({
+    service: 'facebook',
+    appId: 'XXX',
+    secret: 'XXX'
+});
