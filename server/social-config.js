b4a2d966379099cd6b3267d592f3353dc8eff3ac
Jean-Yves Delmotte <jean-yves.delmotte@greencom-networks.com>
2016-01-03 18:10:16 +0100

    ignore some files

--- server/social-config.js
+++
@@ -1,9 +0,0 @@
-ServiceConfiguration.configurations.remove({
-    service: 'facebook'
-});
-
-ServiceConfiguration.configurations.insert({
-    service: 'facebook',
-    appId: 'XXX',
-    secret: 'XXX'
-});
